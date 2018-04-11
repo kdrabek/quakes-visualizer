bac989b59630099c665c1a4d344653e76a8376b3
Krzysztof Drabek <kdrabek@gmail.com>
2018-04-11 20:27:03 +0200

    Correct marker behaviour.

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,6 +1,5 @@
 import { PropTypes } from 'prop-types';
 import ImmutablePropTypes from 'react-immutable-proptypes';
-import Immutable from 'immutable';
 import classNames from 'classnames/bind';
 import {
   withScriptjs,
@@ -21,21 +20,20 @@ const defaultProps = {};
 
 
 export const Map = withScriptjs(withGoogleMap((props) => {  //eslint-disable-line
-  const { center } = props;
+  const { center, isMarkerShown } = props;
   return (
     <GoogleMap {...props}>
-      { center && <Marker position={center} /> }
+      { isMarkerShown && <Marker position={center} /> }
     </GoogleMap>
   );
 }));
 
 const EnhancedMap = ({ mapCoords }) => {
-  const center = mapCoords || Immutable.fromJS({ lat: 51.28, lng: 0.00 });
-  const centerJS = center.toJS();
+  const center = mapCoords ? mapCoords.toJS() : null;
   return (
     <Map
-      isMarkerShown
-      center={centerJS}
+      isMarkerShown={Boolean(center)}
+      center={center}
       loadingElement={<div className={wrapper} />}
       containerElement={<div className={wrapper} />}
       mapElement={<div className={wrapper} />}
